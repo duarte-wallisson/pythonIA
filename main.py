c1a393a9cbb00b43b244bbbd8248e6fba20c3873
duarte-wallisson <wallisson.elduarte@gmail.com>
2023-01-17 22:55:49 -0300

    Teste de funções das bibliotecas seaborn, matplotlib, plotly

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+base_credit = pd.read_csv('content/credit_data.csv')
 
+# clientId == nominal, age == contínua
+# 0 == pagou, 1 == não pagou
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Aula 17
+# print(base_credit)
+# print(base_credit.head())
+# print(base_credit.tail())
+# print(base_credit.describe())
+# print(base_credit[base_credit['income'] >= 69995.685578])
 
+# Aula 18
+# print(np.unique(base_credit['default'], return_counts=True))
+# print(sns.countplot(x=base_credit['default']))
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('World')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# sns.countplot(x=base_credit['default'])
+# plt.hist(x=base_credit['age'])
+# plt.hist(x=base_credit['income'])
+# plt.hist(x=base_credit['loan'])
+# plt.show()
+grafico = px.scatter_matrix(base_credit, dimensions=['age', 'income', 'loan'], color='default')
+grafico.show()
